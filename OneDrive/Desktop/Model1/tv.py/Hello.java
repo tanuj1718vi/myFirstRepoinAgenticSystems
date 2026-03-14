9a0e896f4fc4d471d76adb16dfd63d8cdd8f0056
tanuj1718vi <negit605@gmail.com>
2026-03-14 17:44:07 +0530

    tabe

--- OneDrive/Desktop/Model1/tv.py/Hello.java
+++ OneDrive/Desktop/Model1/tv.py/Hello.java
@@ -3,6 +3,13 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-    }
+    
 
-}
+
+//String
+//charat
+String name = "Tanuj";
+System.out.println(name);
+//length
+System.out.println(name.length());
+    }}
