a2a2032eea943649b586868f55bfe5834a6dffeb
tanuj1718vi <negit605@gmail.com>
2026-04-26 12:30:05 +0530

    Polish number manager project

---
+++ OneDrive/Desktop/Model1/day3.py
@@ -0,0 +1,31 @@
+# number= int(input("enter a number"))
+
+# if number>0 :
+#   print("positive")
+# elif number<0 :
+#   print("negetive")
+# else :
+#   print("zero")
+# if number%2 ==0 :
+#  print("even")
+# else :
+#   print("odd")
+number=[]
+for i in range (5):
+    try:
+        num=(int(input(f"Enter your number{i+1}:")))
+        number.append(num)
+    except:
+      print("You enter a invalid number")   
+Largest=-9999
+Second_largest=-9999
+for num in number :
+    if num > Largest:
+       Second_largest=Largest
+       Largest=num
+       print("Largest",Largest)
+    elif num > Second_largest and num !=Largest :
+        Second_largest=num
+        print("Second largest",Second_largest)
+    else:
+        print("invalid number")
--- OneDrive/Desktop/Model1/panda.py
+++ OneDrive/Desktop/Model1/panda.py
@@ -1,12 +1,24 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 
+ number=[]
+ try:
+   n=int(input("How many numbers you want to enter:"))
+ except ValueError:
+   print("invalic input")
+   n=0
+ for i in range (n):
+    try:
+        num=(int(input(f"enter your number {i+1}:")))
+        number.append(num)
 
-data={"user":["tanuj","satyarth","satyarth","tanuj"],
-      "product":["laptop","mobile","laptop","mobile"],
-      "price":[1000,500,1000,500]
-      }
-df=pd.DataFrame(data)
-print(df)
-df.plot(x="user", y="price", kind="bar")
-plt.show()
+    except:
+      print("invalid input")
+ if len(number)>0:
+  print(number)
+  print("sum",sum(number))
+  print("min",min(number))
+  print("max",max(number))
+  print("average",sum(number)/len(number))
+  print("sorted",sorted(number))
+
+ else:
+  print("invalid values")
